218ff077cf6448ff5b32cb5f277c9ee8eb8ed7a1
SSDolphinSS <dolphinger@yandex.ru>
2024-05-30 14:52:28 +0300

    Dom. Zadanie ispravil

--- module_2_3.py
+++ module_2_3.py
@@ -1,34 +1,8 @@
 my_list = [42, 69, 0, 322, 13, 99, -5, 9, 8, 7, -6, 5]
-while True:
-    if my_list[0] > 0:
-        print(my_list[0])
-    elif my_list[0] < 0:
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    if my_list[i] < 0:
         break
-    if my_list[1] > 0:
-        print(my_list[1])
-    elif my_list[1] < 0:
-        break
-    if my_list[2] > 0:
-        print(my_list[2])
-    elif my_list[2] < 0:
-        break
-    if my_list[3] > 0:
-        print(my_list[3])
-    elif my_list[3] < 0:
-        break
-    if my_list[4] > 0:
-        print(my_list[4])
-    elif my_list[4] < 0:
-        break
-    if my_list[5] > 0:
-        print(my_list[5])
-    elif my_list[5] < 0:
-        break
-    if my_list[6] > 0:
-        print(my_list[6])
-    elif my_list[6] < 0:
-        break
-    if my_list[7] > 0:
-        print(my_list[7])
-    elif my_list[7] < 0:
-        break
+    i += 1
